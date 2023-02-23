b6d82a1d86cd678de6e8662be1b3ffd09c96d6bc
nuruzadesvr <nuruzade.github@gmail.com>
2023-02-23 15:17:06 +0400

    first commit

---
+++ javascript-3.js
@@ -0,0 +1,11 @@
+const log=console.log;
+
+let number=prompt("ededi daxil et: ");
+if(number==0){
+    log("emeliyyat dayandi")
+
+} else if(number%2===0  && number%6===0){
+    log("aktiv")
+}
+
+ 
